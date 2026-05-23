4d2e7eb1360a51777653d458f8fa3c31e8620ce3
pawanyadav2706 <pawanyadav12582@gmail.com>
2026-05-23 12:42:36 +0530

    Concatenation of Array

---
+++ bubblesort/Quicksort/Leetcode/leetcode 1to 50 days/Concatenationofarray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class ConcatenationofArray {
+    public static void main(String[] args){
+        int [] nums= {1,2,3};
+        Solution s = new Solution();
+        int [] result = s.getConcatenation(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
+
+class Solution{
+    public int[] getConcatenation(int[] nums){
+        int n = nums.length;
+        int [] ans = new int[2*n];
+        for(int i=0; i<n; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        return ans;
+    }
+}
