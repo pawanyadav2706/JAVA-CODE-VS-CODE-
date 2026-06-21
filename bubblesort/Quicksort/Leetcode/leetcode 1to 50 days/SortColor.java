d7c1cdb23684df057d4e06d5a1b66d679d7eaf12
pawanyadav2706 <pawanyadav12582@gmail.com>
2026-06-21 14:03:24 +0530

    sortcolor

---
+++ bubblesort/Quicksort/Leetcode/leetcode 1to 50 days/SortColor.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class SortColor {
+    public static void main(String[] args){
+        // int [] nums = {2,0,2,1,1,0};
+         int [] nums = {2,0,1};
+        Solution s= new Solution();
+        int [] result = s.sortcolor(nums);
+        System.out.println(Arrays.toString(result));
+    }
+}
+class Solution{
+    public int[] sortcolor(int [] nums){
+        int low = 0;
+        int mid = 0;
+        int high = nums.length-1;
+
+        while (mid <= high) {
+            if(nums[mid] == 0){
+                int temp = nums[low];
+                nums[low] = nums[mid];
+                nums[mid] = temp;
+                low++;
+                mid++;
+            }else if(nums[mid] ==1){
+                mid++;
+            }else{
+                int temp = nums[mid];
+                nums[mid] = nums[high];
+                nums[high] = temp;
+                high--;
+            }
+        }
+        return nums;
+    }
+}
