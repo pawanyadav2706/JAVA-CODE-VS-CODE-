a90d12a39f78251b778927bca35cae0209545f9f
pawanyadav2706 <pawanyadav12582@gmail.com>
2026-06-23 15:28:30 +0530

    Shuffle String

--- bubblesort/Quicksort/Leetcode/leetcode 1to 50 days/String/ShuffleString.java
+++ bubblesort/Quicksort/Leetcode/leetcode 1to 50 days/String/ShuffleString.java
@@ -1,15 +1,15 @@
 
 import java.util.*;
-public class ReplaceString {
+public class ShuffleString {
     public static void main(String[] args){
         String s = "codeleet";
         int [] indices = {4,5,6,7,0,1,2,3};
         Solution sol = new Solution();
-        System.out.println(sol.isreplacestring(s, indices));
+        System.out.println(sol.isshufflestring(s, indices));
     }
 }
 class Solution{
-    public String isreplacestring(String s, int [] indices){
+    public String isshufflestring(String s, int [] indices){
             char[] ans = new char[s.length()];
 
             for(int i=0; i<s.length(); i++){
