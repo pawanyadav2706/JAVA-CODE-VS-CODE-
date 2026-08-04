6784c9bffc967560315f9959487212d3c9d54658
pawanyadav2706 <pawanyadav12582@gmail.com>
2026-08-04 15:15:52 +0530

    Divided Two Integer

---
+++ Leetcodebitwise/DividedTwoint.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class DividedTwoint {
+    public static void main(String [] args){
+        int dividend = 10;
+        int divisor = 3;
+        Solution sol = new Solution();
+        System.out.println(sol.divided(dividend, divisor));
+    }
+}
+class Solution {
+    public int divided(int dividend , int divisor){
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+        boolean isnegative = dividend < 0 ^ divisor < 0;
+        dividend = Math.abs(dividend);
+        divisor = Math.abs(divisor);
+        int quotient = 0;
+        while(dividend - divisor >= 0){
+            int currdivisor = divisor;
+            int currquotient = 1;
+            while(dividend - (currdivisor << 1) >= 0){
+                currdivisor = currdivisor << 1;
+                currquotient = currquotient << 1;
+            }
+            quotient += currquotient;
+            dividend -= currdivisor; 
+        }
+        return isnegative ? - quotient : quotient;
+    }
+}
