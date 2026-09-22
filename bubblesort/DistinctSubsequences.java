c14ee1a68b93c074149f2b221c3f07d704a6ad80
pawanyadav2706 <pawanyadav12582@gmail.com>
2026-09-22 11:03:44 +0530

    Distin Subsequesce

---
+++ bubblesort/DistinctSubsequences.java
@@ -0,0 +1,30 @@
+public class DistinctSubsequences {
+    public static void main(String [] args){
+        String s = "rabbbit";
+        String t = "rabbit";
+
+        Solution sol = new Solution();
+        int result = sol.numDistinct(s, t);
+        System.out.println(result);
+    }
+}
+class Solution {
+    public int numDistinct(String s, String t){
+        int n = s.length();
+        int m = t.length();
+        int [][] dp = new int[n + 1][ m + 1];
+        for(int i = 0; i<=n; i++){
+        dp[i][0] = 1;
+        }
+        for(int i = 1; i<=n; i++){
+            for(int j = 1; j<=m; j++){
+                if(s.charAt(i - 1) == t.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j];
+                }else{
+                    dp[i][j] = dp[i  - 1][j];
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
